921476b5a8c4a9d048a294aaba5b5eab8f17a290
Rasmus Antons <mail@rasmusantons.de>
2021-12-05 22:13:16 +0100

    remove parent roles from all parents when solving if necessary

--- discord_bot.py
+++ discord_bot.py
@@ -29,16 +29,17 @@ async def solve_command(ctx, solution):
                 await ctx.respond(messages.confirm_solve.format(level_name=level.name))
                 db.session.add(db.UserSolve(user_id=str(ctx.author.id), level=level))
                 db.session.commit()
-                remove_parent_roles = False
+                remove_parent_roles_from = set()
                 if level.extra_discord_role:
                     await add_role_to_user(ctx.author.id, level.extra_discord_role)
                     # todo: remove discord_role?
                 for child_level in level.child_levels:
                     if child_level.discord_role and discord_utils.has_user_reached(child_level, ctx.author.id):
                         await add_role_to_user(ctx.author.id, child_level.discord_role)
-                        remove_parent_roles = True
-                if remove_parent_roles:
-                    await remove_parent_roles_from_user(ctx.author.id, level)
+                        for parent_level in child_level.parent_levels:
+                            remove_parent_roles_from.add(parent_level)
+                for parent_level in remove_parent_roles_from:
+                    await remove_parent_roles_from_user(ctx.author.id, parent_level)
                 break
         else:
             await ctx.respond(messages.reject_solve)
